c3c0613a287187b9d62b271564f3d95a18bd933f
iurcik123 <80324450+iurcik123@users.noreply.github.com>
2022-03-23 13:50:57 +0200

    Add files via upload
    //Here are some Front End web pages.

---
+++ HTML/Starbucks/processorder.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="robots" content="noindex, nofollow">
+    <meta charset="utf-8">
+    <title>The Starbuzz Bean Machine</title>
+  </head>
+<body>
+
+<div id="header">
+<h1>The Starbuzz Bean Machine</h1>
+</div>
+
+
+<div id="content">
+<p>
+Thanks for your order...  But we didn't get your choice of beans
+or whether they are whole or ground.
+You might want to click the back button to go back
+and try again, otherwise, we won't be able to make
+your Bean Machine order, and that would suck.
+</p>
+</div>
+
+<p>
+Here's what we received from you so far:
+</p>
+<p>
+<strong>Number of bags:</strong> 1 <br>
+</p>
+
+
+</body>
+</html>
+
+
